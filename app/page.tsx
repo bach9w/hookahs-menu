c58671cd8455090bda2d52381ff952e79c4e7e28
bach9w <78446671+bach9w@users.noreply.github.com>
2023-12-10 14:45:12 +0200

    Refactor code for improved performance and readability

--- app/page.tsx
+++ app/page.tsx
@@ -7,90 +7,105 @@ const flavoursHookah = [
 		name: 'Bad Girl',
 		content: 'ГРЕЙПФРУТ И ЛАЙМ',
 		price: 25,
+		src: '/badgirl.jpeg',
 	},
 	{
 		id: 2,
 		name: 'Cleopatra',
 		content: 'ГРЕЙПФРУТ, ЛАЙМ И ЛИМОН',
 		price: 25,
+		src: '/cleopatra.jpeg',
 	},
 	{
 		id: 3,
 		name: 'Ama Girls',
 		content: 'ПОРТОКАЛ, ЛИМОН И МЕНТА',
 		price: 25,
+		src: '/amagirls.jpeg',
 	},
 	{
 		id: 4,
 		name: 'Red Lagoon',
 		content: 'БОДЛИВА КРУША И ЛЕД',
 		price: 25,
+		src: '/redlagoon.jpeg',
 	},
 	{
 		id: 5,
 		name: 'Unforgiven',
 		content: 'БОДЛИВА КРУША',
 		price: 25,
+		src: '/unforgiven.jpeg',
 	},
 	{
 		id: 6,
 		name: 'Cosa Nostra',
 		content: 'СИНИ ПЛОДОВЕ И МЕНТА',
 		price: 25,
+		src: '/cosanostra.jpeg',
 	},
 	{
 		id: 7,
 		name: 'Casanova',
 		content: 'СИНИ ПЛОДОВЕ И ПЪПЕШ',
 		price: 25,
+		src: '/casanova.jpeg',
 	},
 	{
 		id: 8,
 		name: 'Lucifer',
 		content: 'ЗЕЛЕНА ЯБЪЛКА И ЯГОДА',
 		price: 25,
+		src: '/lucifer.jpeg',
 	},
 	{
 		id: 9,
 		name: 'Bonnie & Clyde',
 		content: 'ЗЕЛЕНА ЯБЪЛКА И МЕНТА',
 		price: 25,
+		src: '/bonnieclyde.jpeg',
 	},
 	{
 		id: 10,
 		name: 'Don Hookah',
 		content: 'ДРАКОНОВ ПЛОД',
 		price: 25,
+		src: '/donhookah.jpeg',
 	},
 	{
 		id: 11,
 		name: 'Franklin',
 		content: 'ШАМ ФЪСТЪК И ВАНИЛИЯ',
 		price: 25,
+		src: '/franklin.jpeg',
 	},
 	{
 		id: 12,
 		name: 'African Queen',
 		content: 'БОРОВИНКА, ЯГОДА И ПЪПЕШ',
 		price: 25,
+		src: '/africanqueen.jpeg',
 	},
 	{
 		id: 13,
 		name: 'African King',
 		content: 'МИКС ОТ 26 ВИДА ПЛОД',
 		price: 25,
+		src: '/africanking.jpeg',
 	},
 	{
 		id: 14,
 		name: 'Disco',
 		content: 'ПРАСКОВА И МЕНТА',
 		price: 25,
+		src: '/disco.jpeg',
 	},
 	{
 		id: 15,
 		name: 'TNT',
 		content: 'БОРОВИНКА, МАЛИНА И ДИВА ЯГОДА',
 		price: 25,
+		src: '/tnt.jpeg',
 	},
 ];
 
@@ -105,6 +120,7 @@ export default function Home() {
 						name={flavour.name}
 						content={flavour.content}
 						price={flavour.price}
+						src={flavour.src}
 					/>
 				))}
 			</div>
